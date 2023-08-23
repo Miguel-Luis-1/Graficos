5747318048cdb9c6f233718098182937dbb5b0fb
Miguel-Luis-1 <114661451+Miguel-Luis-1@users.noreply.github.com>
2023-08-23 17:06:44 -0300

    Add files via upload

--- graficos.py
+++ graficos.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 data = load_breast_cancer()
 df = pd.DataFrame(data.data, columns=data.feature_names)
 x = data.data
+data['class'] = df
+
 # Calcula a matriz de correlação
 print(df.corr())
 # Cria um heatmap com todas as features 
@@ -17,16 +19,23 @@ plt.show()
 feature_names = data.feature_names
 num_features = x.shape[1]
 
-#Histograma de cada feature individualmente
+# Histograma de cada feature individualmente
 plt.figure(figsize=(30, 10))
 for i in range(num_features):
     plt.subplot(5, 6, i + 1)
     plt.hist(x[:, i], bins=50, color='blue', alpha=0.7)
     plt.title(f'Histograma de {feature_names[i]}')
-
 plt.tight_layout()
 plt.show()
 
+# Bloxplot de todas as features
+box = data.data[:,0:5]
+sns.boxplot(box)
+plt.show()
 
+# ViolinPlot de todas as features
+sns.violinplot(df.corr())
+plt.show()
 
+# Scatterplot agrupando pelas classes
 
