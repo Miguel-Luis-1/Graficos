f42be86c2b62235f64c7c00ee2d1e06745b68a93
Miguel-Luis-1 <114661451+Miguel-Luis-1@users.noreply.github.com>
2023-08-23 23:15:20 -0300

    Update graficos.py

--- graficos.py
+++ graficos.py
@@ -8,42 +8,78 @@ data = load_breast_cancer()
 df = pd.DataFrame(data.data, columns=data.feature_names)
 x = data.data
 
+while True:
+    print("Qual Grafico você deseja acessar?")
+    print("1 - Heatmap")
+    print("2 - Histograma")
+    print("3 - Boxplot")
+    print("4 - Violin Plot")
+    print("5 - Scatter Plot")
+    print("0 - Finalizar")
+    opcao = input("Opção: ")
+    
+    opcao = int (opcao)
 
-# Calcula a matriz de correlação
-print(df.corr())
-# Cria um heatmap com todas as features 
-sns.heatmap(df.corr())
-plt.title("Heatmap de todas as Features")
-plt.show()
-# Obtém a lista de nomes das features
-feature_names = data.feature_names
-num_features = x.shape[1]
+    if(opcao == 1):
+        # Calcula a matriz de correlação
+        print(df.corr())
+        # Cria um heatmap com todas as features 
+        sns.heatmap(df.corr())
+        plt.title("Heatmap de todas as Features")
+        plt.show()
+        # Obtém a lista de nomes das features
+        feature_names = data.feature_names
+        num_features = x.shape[1]
+        continue
 
-# Histograma de cada feature individualmente
-plt.figure(figsize=(30, 10))
-for i in range(num_features):
-    plt.subplot(5, 6, i + 1)
-    plt.hist(x[:, i], bins=50, color='blue', alpha=0.7)
-    plt.title(f'Histograma de {feature_names[i]}')
-plt.tight_layout()
-plt.show()
+    elif(opcao == 2):
+        # Histograma de cada feature individualmente
+        plt.figure(figsize=(30, 10))
+        for i in range(num_features):
+            plt.subplot(5, 6, i + 1)
+            plt.hist(x[:, i], bins=50, color='blue', alpha=0.7)
+            plt.title(f'Histograma de {feature_names[i]}')
+        plt.tight_layout()
+        plt.show()
+        continue
 
-# Bloxplot de todas as features
-box = data.data[:,0:5]
-sns.boxplot(box)
-plt.show()
+    elif(opcao == 3):
+        # Bloxplot de todas as features
+        box = data.data[:,0:5]
+        sns.boxplot(box)
+        plt.show()
+        continue
 
-# ViolinPlot de todas as features
-sns.violinplot(df.corr())
-plt.show()
+    elif(opcao == 4):
+        # ViolinPlot de todas as features
+        sns.violinplot(df.corr())
+        plt.show()
+        continue
 
-# Scatterplot agrupando pelas classes
-dt = load_breast_cancer()
-dataset = pd.DataFrame(data=dt.data, columns=dt.feature_names)
-dataset['class'] = dt.target
+    elif(opcao == 5):
+        # Scatterplot agrupando pelas classes
+        dt = load_breast_cancer()
+        dataset = pd.DataFrame(data=dt.data, columns=dt.feature_names)
+        dataset['class'] = dt.target
 
-# Gráfico de dispersão agrupando pelas classes
-graphic = sns.PairGrid(dataset, hue = 'class')
-graphic.map_offdiag(sns.scatterplot)
-graphic.add_legend()
-plt.show()
+        graphic = sns.PairGrid(dataset, hue = 'class')
+        graphic.map_offdiag(sns.scatterplot)
+        graphic.add_legend()
+        plt.show()
+        continue
+    
+    elif(opcao == 0):
+        break
+    else:
+        print("Digite um número valido!!")
+
+
+
+
+
+
+
+
+
+
+
